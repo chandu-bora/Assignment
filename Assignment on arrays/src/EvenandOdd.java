afba05964a577dd8a95528be9ae604bc5d2eb6e3
poorna <poorna@toucan-l009>
2022-06-23 15:32:13 +0530

    Updated assignment

--- Assignment on arrays/src/EvenandOdd.java
+++ Assignment on arrays/src/EvenandOdd.java
@@ -1,6 +1,5 @@
 
 public class EvenandOdd {
-
 	public static void main(String[] args) {
 		int a[]= {1,2,3,4,5,6};
 		System.out.println("Even number are: ");
@@ -15,7 +14,5 @@ public class EvenandOdd {
 			if (a[i]%2!=0)
 				System.out.println(a[i]);
 	}
-
 }
 }
-
